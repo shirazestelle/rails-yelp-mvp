8e2919ef3ea9b5c9773c249899505f53adab0913
Shiraz Kittelberger-Buehlbecker <buehlbeckershiraz@gmail.com>
2018-05-16 17:13:14 +0200

    First exercice with css on home

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = [
+     {
+        name:         'Dishoom',
+        address:      '7 Boundary St, London E2 7JE',
+        category:     'chinese'
+      },
+      {
+         name:         'Pizza Pilgrims',
+         address:      'Kingley Street, Soho, London ',
+         category:     'italian'
+       },
+       {
+          name:         'Tsuanami',
+          address:      'Clapham Common, London',
+          category:     'japanese'
+        },
+        {
+           name:         'Hakassan',
+           address:      'Berkley Square, London',
+           category:     'chinese'
+         },
+       {
+          name:         'Brasserie Zedel',
+          address:      '20 Sherwood St, Soho, London W1F 7ED',
+          category:     'french'
+        },
+]
+
+Restaurant.create!(restaurants)
+
+
+
+
+
+
+
+
